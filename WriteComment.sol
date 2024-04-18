b774bd0941f64e06043d91bd5ce8f47676bc10d1
Hojin Kim <miu.null@gmail.com>
2024-04-19 00:18:10 +0900

    write ERC-1155 contract

--- WriteComment.sol
+++ WriteComment.sol
@@ -1,12 +1,44 @@
 //SPDX-Liscense-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WriteComment is ERC721Enumerable {
-    constructor() ERC721("comment","COM") {}
-    function writeYourComment() public {
-        uint256 commentTokenId = totalSupply() + 1;
-    }
+
+//import "hardhat/console.sol";
+//import { Base64 } from "./libraries/Base64.sol";
+
+contract WriteComment is ERC1155i, Ownable {
+   using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    string private _baseURIextended;
+
+    constructor(string memory baseURI) ERC1155i("comment","COM") {}
+    setBaseURI(baseURI);
     
+    //only owner can set BaseURI
+    function setBaseURI(string memory baseURI) public onlyOwner{
+        _baseURIextended = baseURI;
+    }
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURIextended;
+    }
+
+     // Override uri method to append token-specific part to the URI.
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseURIextended, Strings.toString(tokenId), ".json"));
+    }
+
+    //mint
+    function writeYourComment(string memory tokenURI,uint256 amount) public returns (uint256) {
+        _tokenIdCounter.increment();
+        uint256 commentTokenId = _tokenIdCounter.current();
+    
+    _mint(msg.sender, commentTokenId);
+    _setTokenURI(commentTokenId, tokenURI);
+
+    return commentTokenId;
+    }
+
 }
